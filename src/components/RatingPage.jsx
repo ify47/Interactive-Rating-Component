96294250960f2bf8060b403710a2bf4319ffbe9f
Ifeanyi Ofikulu <ifeanyiofi@gmail.com>
2023-06-27 18:24:52 +0100

    commit message

--- src/components/RatingPage.jsx
+++ src/components/RatingPage.jsx
@@ -2,21 +2,23 @@ import React, { useState } from "react";
 import star from "../images/icon-star.svg";
 
 const RatingPage = ({ rateNumber, classAction }) => {
+  //props from parent component (app.js)
   const [inputs, setInputs] = useState("");
+  const rates = [1, 2, 3, 4, 5];
 
   const handleChange = (e) => {
     setInputs(e.target.value);
   };
 
   const handleSubmit = () => {
-    rateNumber(inputs);
+    rateNumber(inputs); // passing the inputs value as a child to parent component
   };
 
   return (
     <section
       className={`bg-[linear-gradient(hsl(213,19%,18%),hsl(216deg_14.91%_11.79%))] w-[min(100%_-_33px,400px)] p-[30px] rounded-[25px] ${
         classAction ? "hidden" : ""
-      }`}
+      }`} //checking the boolean value provided from props if its true and adds hidden class from tailwind, if it's not leave it empty
     >
       <img
         className="bg-[hsl(216deg_17.83%_24.33%)] w-10 p-2.5 rounded-[50%]"
@@ -31,59 +33,23 @@ const RatingPage = ({ rateNumber, classAction }) => {
         appreciated to help us improve our offering!
       </p>
       <div className="flex justify-between mt-5">
-        <input
-          type="radio"
-          name="number"
-          id="one"
-          value="1"
-          onChange={handleChange}
-        />
-        <label className="numberbuttons" htmlFor="one">
-          1
-        </label>
-        <input
-          type="radio"
-          name="number"
-          id="two"
-          value="2"
-          onChange={handleChange}
-        />
-        <label className="numberbuttons" htmlFor="two">
-          2
-        </label>
-        <input
-          type="radio"
-          name="number"
-          id="three"
-          value="3"
-          onChange={handleChange}
-        />
-        <label className="numberbuttons" htmlFor="three">
-          3
-        </label>
-        <input
-          type="radio"
-          name="number"
-          id="four"
-          value="4"
-          onChange={handleChange}
-        />
-        <label className="numberbuttons" htmlFor="four">
-          4
-        </label>
-        <input
-          type="radio"
-          name="number"
-          id="five"
-          value="5"
-          onChange={handleChange}
-        />
-        <label className="numberbuttons" htmlFor="five">
-          5
-        </label>
+        {rates.map((rates) => (
+          <div key={rates}>
+            <input
+              type="radio"
+              name="number"
+              id={rates}
+              value={rates}
+              onChange={handleChange}
+            />
+            <label className="numberbuttons" htmlFor={rates}>
+              {rates}
+            </label>
+          </div>
+        ))}
       </div>
-      <button disabled={!inputs} onClick={handleSubmit}>
-        SUBMIT
+      <button className="uppercase" disabled={!inputs} onClick={handleSubmit}>
+        submit
       </button>
     </section>
   );
